dd34a7b7ca9171a15abd07e7ff52d65a9c7f8c2f
Philipp Küng <git@philippkueng.ch>
2019-06-08 00:05:18 +0200

    replicating the base error case

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -1,10 +1,19 @@
 
 (ns app.main
-  (:require [app.lib :as lib]))
+  (:require [app.lib :as lib]
+            ["lucene-tokenizers-for-bitfondue" :refer [tokenize_js]]
+            ["dexie" :refer [Dexie]]))
 
 (def a 1)
 
 (defonce b 2)
+
+(def ^:private db (do (new Dexie "example-db")))
+
+(defn- tokenize
+  "Turn the provided text into tokens"
+  [text]
+  (js->clj (tokenize_js text) :keywordize-keys true))
 
 (defn main! []
   (println "[main]: loading"))
